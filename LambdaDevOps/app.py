b0dfd0abdff24b1093301d12ea725381c8f2b2dc
haya-alnafisah <alnafisah.haya@gmail.com>
2022-06-08 15:37:54 +0300

    first project

---
+++ LambdaDevOps/app.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+def lambda_handler(event, context):
+  # print (event)
+  statusCode = 200
+  return {
+    "statusCode": statusCode,
+    "body": json.dumps(["ansible", "jenkins", "docker", "k8s"]),
+    "headers": {
+      "Content-Type": "application/json"
+    }
+  }
